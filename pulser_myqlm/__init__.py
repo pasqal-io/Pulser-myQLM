c25d5eed4146f734edddf251dab8af3310d0c7a6
a_corni <antoine.cornillot@pasqal.com>
2024-01-12 12:54:03 +0100

    Create FresnelDevice

--- pulser_myqlm/__init__.py
+++ pulser_myqlm/__init__.py
@@ -1,3 +1,4 @@
 """An extension to execute MyQLM jobs on Pasqal devices."""
 from pulser_myqlm._version import __version__
+from pulser_myqlm.devices import FresnelDevice
 from pulser_myqlm.pulserAQPU import FresnelQPU, IsingAQPU
