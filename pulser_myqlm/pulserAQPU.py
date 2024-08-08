51b241c63acdc2b762115fd89a3c357e690c037b
Augustinio <augustinlg.dev@gmail.com>
2024-08-08 16:43:54 +0200

    try this

--- pulser_myqlm/pulserAQPU.py
+++ pulser_myqlm/pulserAQPU.py
@@ -2,14 +2,20 @@
 
 import warnings
 
-from pulser_myqlm.fresnel_qpu import FresnelQPU  # noqa F401
-from pulser_myqlm.ising_aqpu import IsingAQPU  # noqa F401
+from pulser_myqlm.fresnel_qpu import FresnelQPU
+from pulser_myqlm.ising_aqpu import IsingAQPU
+
+
+__all__ = ['FresnelQPU', 'IsingAQPU']
+
 
 warnings.warn(
-    "This module is deprecated. You should import from"
-    + "pulser_myqlm.frensel_qpu or pulser_myqlm.ising_aqpu"
-    + "instead. Support for imports from this module will stop"
-    + "in version 1.0.0 of this library.",
+    (
+        "This module is deprecated. Please import from "
+        "pulser_myqlm.fresnel_qpu or pulser_myqlm.ising_aqpu instead. "
+        "Support for imports from this module will be removed in "
+        "version 1.0.0 of this library."
+    ),
     DeprecationWarning,
     stacklevel=2,
 )
