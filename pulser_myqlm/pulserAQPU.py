b1f901e5b48e0deb539e69c82c8cb291f98af985
Augustinio <augustinlg.dev@gmail.com>
2024-08-08 16:12:36 +0200

    formatting

--- pulser_myqlm/pulserAQPU.py
+++ pulser_myqlm/pulserAQPU.py
@@ -1,11 +1,15 @@
+"""Legacy import path."""
+
 import warnings
 
-from pulser_myqlm.fresnel_qpu import FresnelQPU
-from pulser_myqlm.ising_aqpu import IsingAQPU
-
+from pulser_myqlm.fresnel_qpu import FresnelQPU  # noqa F401
+from pulser_myqlm.ising_aqpu import IsingAQPU  # noqa F401
 
 warnings.warn(
-    "This module is deprecated. Do not import from here",
+    "This module is deprecated. You should import from"
+    + "pulser_myqlm.frensel_qpu or pulser_myqlm.ising_aqpu"
+    + "instead. Support for imports from this module will stop"
+    + "in version 1.0.0 of this library.",
     DeprecationWarning,
     stacklevel=2,
 )
--- tests/test_compatiblity.py
+++ tests/test_compatiblity.py
@@ -1,9 +1,11 @@
 import pytest
 
+
 def test_legacy_imports():
-    """
+    """Backwards compatibility test.
+
     Test that we can still import FresnelQPU and IsingAQPU from the old
     PulserAQPU module with a deprecation warning.
     """
     with pytest.warns(DeprecationWarning):
-        from pulser_myqlm.PulserAQPU import IsingAQPU, FresnelQPU
+        from pulser_myqlm.PulserAQPU import FresnelQPU, IsingAQPU  # noqa F401
