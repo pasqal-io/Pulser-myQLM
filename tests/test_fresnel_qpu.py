5c9b19ba3d72651f1690f32c970d575149fd4419
a_corni <antoine.cornillot@pasqal.com>
2024-08-28 17:07:29 +0200

    Coming back to sleeping time of 5

--- tests/test_fresnel_qpu.py
+++ tests/test_fresnel_qpu.py
@@ -398,7 +398,7 @@ def test_non_operational_qpu(
             else nullcontext()
         ):
             server_thread.start()
-        time.sleep(3)
+        time.sleep(5)
     qpu = get_remote_qpu(PORT) if remote_fresnel else fresnel_qpu
 
     # Simulate Sequence using Pulser Simulation
