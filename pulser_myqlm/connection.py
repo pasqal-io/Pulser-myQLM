76f1009afda7ba242b150380337815febe713edd
a_corni <antoine.cornillot@pasqal.com>
2025-04-07 17:50:58 +0200

    Fixing connection

--- pulser_myqlm/connection.py
+++ pulser_myqlm/connection.py
@@ -1,4 +1,5 @@
 """Defines the PulserMyQLMBackend."""
+
 from __future__ import annotations
 
 import time
@@ -6,17 +7,59 @@ import typing
 
 import pulser
 import qat
-from qat.comm.qlmaas.ttypes import QLMServiceException
+from pulser.backend.remote import BatchStatus, JobStatus
 from pulser.json.exceptions import DeserializeDeviceError
+from qat.comm.qlmaas.ttypes import QLMServiceException
 
+from pulser_myqlm.helpers.deserialize_other import deserialize_other
 from pulser_myqlm.ising_aqpu import IsingAQPU
-from pulser_myqlm.fresnel_qpu import FresnelQPU
+
+JOB_STATUS_QLM_TO_PULSER_BATCH: dict[qat.comm.qlmaas.ttypes.JobStatus, BatchStatus] = {
+    qat.comm.qlmaas.ttypes.JobStatus.WAITING: BatchStatus.PENDING,
+    qat.comm.qlmaas.ttypes.JobStatus.RUNNING: BatchStatus.RUNNING,
+    qat.comm.qlmaas.ttypes.JobStatus.DONE: BatchStatus.DONE,
+    qat.comm.qlmaas.ttypes.JobStatus.CANCELLED: BatchStatus.CANCELED,
+    qat.comm.qlmaas.ttypes.JobStatus.UNKNOWN_JOB: BatchStatus.ERROR,
+    qat.comm.qlmaas.ttypes.JobStatus.IN_BUCKET: BatchStatus.PENDING,
+    qat.comm.qlmaas.ttypes.JobStatus.DELETED: BatchStatus.ERROR,
+    qat.comm.qlmaas.ttypes.JobStatus.STOPPED: BatchStatus.CANCELED,
+    qat.comm.qlmaas.ttypes.JobStatus.FAILED: BatchStatus.ERROR,
+}
+
+JOB_STATUS_QLM_TO_PULSER_JOB: dict[qat.comm.qlmaas.ttypes.JobStatus, JobStatus] = {
+    qat.comm.qlmaas.ttypes.JobStatus.WAITING: JobStatus.PENDING,
+    qat.comm.qlmaas.ttypes.JobStatus.RUNNING: JobStatus.RUNNING,
+    qat.comm.qlmaas.ttypes.JobStatus.DONE: JobStatus.DONE,
+    qat.comm.qlmaas.ttypes.JobStatus.CANCELLED: JobStatus.CANCELED,
+    qat.comm.qlmaas.ttypes.JobStatus.UNKNOWN_JOB: JobStatus.ERROR,
+    qat.comm.qlmaas.ttypes.JobStatus.IN_BUCKET: JobStatus.PENDING,
+    qat.comm.qlmaas.ttypes.JobStatus.DELETED: JobStatus.ERROR,
+    qat.comm.qlmaas.ttypes.JobStatus.STOPPED: JobStatus.CANCELED,
+    qat.comm.qlmaas.ttypes.JobStatus.FAILED: JobStatus.ERROR,
+}
+
 
 class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
-    """The connection to a MyQLM QPU."""
+    """A connection to a QLM, to submit Sequences to QPUs.
 
-    def __init__(self, connection: qat.qlmaas.QLMaaSConnection):
-        self._connection = connection
+    Wraps qat.qlmaas.QLMaaSConnection to:
+        - connect to a QLM.
+        - Find the QPUs connected to this QLM that accept Sequences.
+        - Find the pulser Device implemented by this QPU at the time
+            of the connection.
+        - Submit pulser Sequences to the QPU, for execution.
+        - Retrieve the Results of the execution as pulser results.
+
+    Keyword Arguments:
+        The arguments to initialize a qat.qlmaas.QLMaaSConnection.
+
+    Attribute:
+        _connection: The QLMaaSConnection instantiated with the keyword
+            arguments.
+    """
+
+    def __init__(self, **kwargs: dict[str, typing.Any]) -> None:
+        self._connection = qat.qlmaas.QLMaaSConnection(**kwargs)
 
     def supports_open_batch(self) -> bool:
         """Flag to confirm this class doesn't support open batch creation."""
@@ -26,6 +69,24 @@ class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
     def _get_job_ids_from_batch(batch: qat.core.Batch) -> list[str]:
         """Generate the job IDs for a qat.core.Batch."""
         return [f"{i}" for (i, _) in enumerate(batch.jobs)]
+
+    @staticmethod
+    def _convert_qlm_status_to_pulser_batch(
+        job_status: qat.qlmaas.ttypes.JobStatus,
+    ) -> BatchStatus:
+        try:
+            return JOB_STATUS_QLM_TO_PULSER_BATCH[job_status]
+        except IndexError as e:
+            raise ValueError(f"Unknown Job status {job_status}.") from e
+
+    @staticmethod
+    def _convert_qlm_status_to_pulser_job(
+        job_status: qat.qlmaas.ttypes.JobStatus,
+    ) -> JobStatus:
+        try:
+            return JOB_STATUS_QLM_TO_PULSER_JOB[job_status]
+        except IndexError as e:
+            raise ValueError(f"Unknown Job status {job_status}.") from e
 
     def submit(
         self,
@@ -46,7 +107,7 @@ class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
         )
         mimic_qpu: bool = kwargs.get("mimic_qpu", False)
         if mimic_qpu:
-            sequence = self.update_sequence_device(sequence, job_params)
+            sequence = self.update_sequence_device(sequence)
             pulser.QPUBackend.validate_job_params(job_params, sequence.device.max_runs)
 
         if sequence.is_parametrized() or sequence.is_register_mappable():
@@ -86,11 +147,9 @@ class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
         res_info = async_res.get_info()
         if wait:
             async_res.join()
-        
+
         return pulser.backend.remote.RemoteResults(
-            res_info.id,
-            self,
-            PulserQLMConnection._get_job_ids_from_batch(batch)
+            res_info.id, self, PulserQLMConnection._get_job_ids_from_batch(batch)
         )
 
     def fetch_available_devices(self) -> dict[str, pulser.devices.Device]:
@@ -98,9 +157,6 @@ class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
         qpus = self._connection.get_qpus()
         devices = {}
         for qpu in qpus:
-            if "PasqalQPU" in qpu.name: 
-                devices[qpu.name] = FresnelQPU(None).device
-                continue
             try:
                 real_qpu = self._connection.get_qpu(qpu)()
             except RuntimeError:
@@ -116,27 +172,32 @@ class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
 
     def _fetch_result(
         self, batch_id: str, job_ids: list[str] | None
-    ) -> typing.Sequence[qat.core.BatchResult | qat.core.Result]:
-        """Fetches the results of a completed batch."""
-        # Returns an error if job is not available
-        results = self._connection.get_job(batch_id).get_result()
+    ) -> tuple[pulser.result.Result, ...]:
+        # The results are always sampled results
+        jobs = self._query_job_progress(batch_id)
+
         if job_ids is None:
-            return results
-        batch_jobs_ids = self._get_job_ids(batch_id)
-        sub_results = []
-        for job_id in job_ids:
-            try:
-                job_idx_in_batch = batch_job_ids.index(job_id)
-            except ValueError as e:
+            job_ids = list(jobs.keys())
+
+        results: list[pulser.result.Result] = []
+        for id in job_ids:
+            if id not in jobs:
                 raise ValueError(
-                    f"Job id {job_id} is not among Job ids of the batch {batch_job_ids}"
-                ) from e
-            sub_results.append(results[job_idx_in_batch])
-        return qat.core.BatchResult(results=sub_results, meta_data=results.meta_data)
+                    f"Job id {id} is invalid, valid ids are {list(jobs.keys())}."
+                )
+            status, result = jobs[id]
+            if status != JobStatus.DONE:
+                raise pulser.backend.remote.RemoteResultsError(
+                    f"The results are not yet available, job {id} status is {status}."
+                )
+            assert result is not None  # result is None if its status is not DONE
+            results.append(result)
+
+        return tuple(results)
 
     def _query_job_progress(
         self, batch_id: str
-    ) -> typing.Mapping[str, tuple[qat.comm.qlmaas.ttypes.JobStatus, qat.core.Result | None]]:
+    ) -> typing.Mapping[str, tuple[JobStatus, pulser.result.Result | None]]:
         """Fetches the status and results of all the jobs in a batch.
 
         Unlike `_fetch_result`, this method does not raise an error if some
@@ -144,41 +205,76 @@ class PulserQLMConnection(pulser.backend.remote.RemoteConnection):
 
         It returns a dictionary mapping the job ID to its status and results.
         """
-        status = self._get_batch_status(batch_id)
-        job_ids = self._get_job_ids(batch_id)
-        if status != qat.comm.qlmaas.ttypes.JobStatus.DONE:
-            return {job_id:(status, None) for job_id in job_ids}
-        results = self._connection.get_job(batch_id).get_result()
-        return {job_ids[i]:(status, result) for (i, result) in enumerate(results.results)}
+        qlm_batch = self._get_batch(batch_id)
+        qlm_status = self._connection.get_job(batch_id).get_status(human_readable=False)
+        status = PulserQLMConnection._convert_qlm_status_to_pulser_job(qlm_status)
+        job_ids = PulserQLMConnection._get_job_ids_from_batch(qlm_batch)
+        if status != JobStatus.DONE:
+            return {job_id: (status, None) for job_id in job_ids}
+        results: dict[str, tuple[JobStatus, pulser.result.Result | None]] = {}
+        for i, result in enumerate(
+            self._connection.get_job(batch_id).get_result().results
+        ):
+            job = qlm_batch[i]
+            job_id = job_ids[i]
+            if job.schedule is None:
+                raise pulser.backend.remote.RemoteResultsError(
+                    f"The Job {job_id} does not have a schedule: "
+                    "it can't have run on the QPU.",
+                )
+            try:
+                other_dict = deserialize_other(job.schedule._other)
+            except ValueError as e:
+                raise pulser.backend.remote.RemoteResultsError(
+                    "Failed at finding a Sequence in the schedule" f"of Job {job_id}.",
+                ) from e
+            seq = other_dict["seq"]
+            results[job_id] = (
+                status,
+                pulser.result.SampledResult(
+                    atom_order=seq.get_register(include_mappable=True).qubit_ids,
+                    meas_basis=seq.get_measurement_basis(),
+                    bitstring_counts=IsingAQPU.convert_result_to_samples(result),
+                ),
+            )
+        return results
 
-    def _get_batch_status(self, batch_id: str) -> qat.comm.qlmaas.ttypes.JobStatus:
+    def _get_batch_status(self, batch_id: str) -> BatchStatus:
         """Gets the status of a batch from its ID."""
-        return self._connection.get_job(batch_id).get_status(human_readable=False)
+        qlm_status = self._connection.get_job(batch_id).get_status(human_readable=False)
+        return PulserQLMConnection._convert_qlm_status_to_pulser_batch(qlm_status)
+
+    def _get_batch(self, batch_id: str) -> qat.core.Batch:
+        """Returns the myQLM Batch associated with a batch id."""
+        qlm_batch = None
+        for i in range(10):
+            try:
+                qlm_batch = self._connection.get_job(batch_id).get_batch()
+                break
+            except QLMServiceException:
+                time.sleep(1)
+        if qlm_batch is None:
+            raise QLMServiceException(
+                f"Max retries performed while trying to access batch of {batch_id}."
+            )
+        return qlm_batch
 
     def _get_job_ids(self, batch_id: str) -> list[str]:
         """Gets all the job IDs within a batch."""
-        qlm_batch = None
-        for i in range(10):
-            try: 
-                qlm_batch = self._connection.get_job(batch_id).get_batch()
-                break
-            except QLMServiceException as e:
-                time.sleep(1)
-        if qlm_batch is None:
-            raise QLMServiceException("Max retries performed")
+        qlm_batch = self._get_batch(batch_id)
         return PulserQLMConnection._get_job_ids_from_batch(qlm_batch)
 
     def _close_batch(self, batch_id: str) -> None:
         """Closes a batch using its ID."""
         raise NotImplementedError(  # pragma: no cover
-            "Unable to close batch through this remote connection"
+            "No batch can be open/closed through this remote connection."
             "It can be cancelled using `cancel_batch`."
         )
 
     def cancel_batch(self, batch_id: str) -> None:
         """Cancels a batch using its ID."""
-        return self._connection.get_job(batch_id).cancel()
+        self._connection.get_job(batch_id).cancel()
 
     def delete_batch(self, batch_id: str) -> None:
         """Deletes the files of a batch using its ID."""
-        return self._connection.get_job(batch_id).delete_files()
+        self._connection.get_job(batch_id).delete_files()
