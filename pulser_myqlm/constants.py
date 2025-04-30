143e9bff99141d59cde55d979be235b0b864f66e
a_corni <antoine.cornillot@pasqal.com>
2025-04-30 14:46:20 +0200

    create envs variables for constants

--- pulser_myqlm/constants.py
+++ pulser_myqlm/constants.py
@@ -1,16 +1,17 @@
 """Defines common constants."""
 
+import os
 from pathlib import Path
 from typing import cast
 
 from pulser.devices._device_datacls import Device
 from pulser.json.abstract_repr.deserializer import deserialize_device
 
-DEFAULT_NUMBER_OF_SHOTS = 2000
-QPU_POLLING_INTERVAL_SECONDS = 5
-JOB_POLLING_INTERVAL_SECONDS = 1
-MAX_CONNECTION_ATTEMPTS_QLM = 5
-MAX_CONNECTION_ATTEMPTS_QPU = 5
+DEFAULT_NUMBER_OF_SHOTS = os.getenv("DEFAULT_NUMBER_OF_SHOTS", 2000)
+QPU_POLLING_INTERVAL_SECONDS = os.getenv("QPU_POLLING_INTERVAL_SECONDS", 5)
+JOB_POLLING_INTERVAL_SECONDS = os.getenv("QPU_POLLING_INTERVAL_SECONDS", 1)
+MAX_CONNECTION_ATTEMPTS_QLM = os.getenv("MAX_CONNECTION_ATTEMPS_QLM", 5)
+MAX_CONNECTION_ATTEMPTS_QPU = os.getenv("MAX_CONNECTION_ATTEMPTS_QPU", 5)
 
 with open(Path(__file__).parent / "temp_device.json", "r", encoding="utf-8") as f:
     TEMP_DEVICE = cast(Device, deserialize_device(f.read()))
